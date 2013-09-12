5a16c15c1c200d324dac10ed023ab1ffa3ed8e44
EliseoRios <eliseohack@gmail.com>
2013-09-12 00:55:23 -0500

    Finalizado...

---
+++ ConversorDolar.php
@@ -0,0 +1,15 @@
+<html>
+ <head>
+  <title>Conversion</title>
+ </head>
+
+ <body>
+  <?php
+   $Dolares=12.5;
+   $TasaCambio=15.5;
+   $ConversionPesos=$Dolares*$TasaCambio;
+   
+   echo $Dolares." Dolares equivale a $".$ConversionPesos." pesos";
+  ?>
+ </body>
+</html>
